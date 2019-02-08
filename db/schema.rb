ab126920ebdff0c16febe4bdd148159bc046be4c
Jonathan Johnson <me@jondavidjohn.com>
2019-02-08 16:12:13 -0600

    db bump

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_08_211344) do
+ActiveRecord::Schema.define(version: 2019_02_08_221017) do
 
   create_table "addresses", force: :cascade do |t|
     t.text "name"
